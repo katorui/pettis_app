a508e34976cb4beb599540f53334780ebd60d8e2
加藤瑠依 <rui14jpn@gmail.com>
2021-05-09 21:28:49 +0900

    first commit

---
+++ Auth/register.php
@@ -0,0 +1,17 @@
+<h1>新規会員登録</h1>
+<form action="register_comfirm.php" method="post">
+    <div>
+        <label>名前：<label>
+        <input type="text" name="name">
+    </div>
+    <div>
+        <label>メールアドレス：<label>
+        <input type="text" name="mail">
+    </div>
+    <div>
+        <label>パスワード：<label>
+        <input type="password" name="password">
+    </div>
+    <input type="submit" value="新規登録">
+</form>
+<p>すでに登録済みの方は<a href="login.php">こちら</a></p>
